e0b4e722dc49d422018785a57de5b8f86694476e
naruto-dk <2235693426@qq.com>
2017-04-21 19:27:40 +0800

    dr

---
+++ Class-note/01HTML/表单/service.php
@@ -0,0 +1,8 @@
+<?php
+echo "hello world <br/>";
+echo "账号：".$_GET["name"]."<br/>";
+echo "账号：".$_GET["password"]."<br/>";
+
+//echo "账号：".$_POST["name"]."<br/>";
+//echo "账号：".$_POST["password"]."<br/>";
+?>
